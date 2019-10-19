9889e65aa17904af77f463f40b24c2ff45928e21
Fabiano Gadenz <fabianogadenz@MacBook-Pro-de-Fabiano.local>
2019-10-19 15:46:49 -0300

    criando repo

---
+++ android/app/src/main/kotlin/br/com/speedcode/app_nasa/MainActivity.kt
@@ -0,0 +1,13 @@
+package br.com.speedcode.app_nasa
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
